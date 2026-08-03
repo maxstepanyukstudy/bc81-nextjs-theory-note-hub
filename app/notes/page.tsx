9118219f876f995098901f444006433cdb366ccc
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-08-03 21:28:53 +0300

    Notes page fetch notes from api

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,10 +1,14 @@
 import { Metadata } from "next";
+import { getNotes } from "@/lib/api";
 
 export const metadata: Metadata = {
   title: "Notes Page",
   description: "Notes Page ...",
 };
 
-export default function Notes() {
+export default async function Notes() {
+  const notes = await getNotes();
+  console.log("notes", notes);
+
   return <div>Notes</div>;
 }
