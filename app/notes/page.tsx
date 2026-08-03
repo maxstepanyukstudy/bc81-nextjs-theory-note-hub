9da4220853200479eba8e5a20d5ab14a80b80736
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-08-03 22:38:24 +0300

    Notes - use client

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,19 +1,31 @@
+"use client";
+
 import { Metadata } from "next";
-import { getNotes } from "@/lib/api";
+import { getNotes, Note } from "@/lib/api";
 import NoteList from "@/components/NoteList/NoteList";
+import { useState } from "react";
 
-export const metadata: Metadata = {
-  title: "Notes Page",
-  description: "Notes Page ...",
-};
+// // cant use during "use client"; 
+// export const metadata: Metadata = {
+//   title: "Notes Page",
+//   description: "Notes Page ...",
+// };
 
-export default async function Notes() {
-  const response = await getNotes();
+export default function Notes() {
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  const handleClick = async () => {
+    const response = await getNotes();
+    if (response?.notes) {
+      setNotes(response.notes);
+    }
+  };
 
   return (
     <section>
       <h1>Notes List</h1>
-      {response?.notes?.length > 0 && <NoteList notes={response.notes} />}
+      <button onClick={handleClick}>Get my notes</button>
+      {notes.length > 0 && <NoteList notes={notes} />}
     </section>
   );
 }
