80e4188b49531b08af31f21a7bc41135493740dc
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-08-03 20:58:18 +0300

    upd RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,8 +23,20 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <header>
+          <h1>Hello Note Hub</h1>
+        </header>
+
+        <main>{children}</main>
+
+        <footer>
+          <p>
+            Created <time dateTime="2025">2025</time>
+          </p>
+        </footer>
+      </body>
     </html>
   );
 }
